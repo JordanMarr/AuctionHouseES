44870680f3bbc803451f11bbe6ad6d3c411385cb
Jordan Marr <JordanMarr@users.noreply.github.com>
2023-01-30 17:49:56 -0500

    Added minimal api

---
+++ src/WebApi/Program.fs
@@ -0,0 +1,8 @@
+﻿open Microsoft.AspNetCore.Builder
+open System
+
+let builder = WebApplication.CreateBuilder()
+let app = builder.Build()
+
+app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+app.Run()
