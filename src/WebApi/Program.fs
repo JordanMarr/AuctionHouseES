d6cba5e0fb05a8a8a16011151eb24d09d0dfd54b
Jordan Marr <JordanMarr@users.noreply.github.com>
2023-01-30 18:51:04 -0500

    Added Marten and wired to postgres

--- src/WebApi/Program.fs
+++ src/WebApi/Program.fs
@@ -1,9 +1,20 @@
 ﻿open Microsoft.AspNetCore.Builder
 open Giraffe
+open Marten
 
 let webApp =
     choose [ route "/" >=> text "Hello world!" ]
 
-let app = WebApplication.CreateBuilder().Build()
+let builder = WebApplication.CreateBuilder()
+
+builder.Services.AddMarten(fun (opts: StoreOptions) -> 
+    let connectionString = "Server=localhost;Port=54320;Database=postgres;User Id=postgres;Password=example;Timeout=3"
+    opts.Connection(connectionString) 
+    |> ignore
+)
+|> ignore
+
+let app = builder.Build()
 app.UseGiraffe webApp
+
 app.Run()
