ed2b916443e8700df0de2cb1dd6bee079467f200
Jordan Marr <JordanMarr@users.noreply.github.com>
2023-01-30 18:07:15 -0500

    Added AuctionHouseES project with auction Events

---
+++ src/AuctionHouseES/Events.fs
@@ -0,0 +1,25 @@
+﻿module AuctionHouseES.Events
+
+open System
+
+type AuctionId = Guid
+type UserId = Guid
+
+type AuctionCreated = 
+    {
+        Id: AuctionId
+        StartedBy: UserId
+        StartedOn: DateTimeOffset
+        EndsOn: DateTimeOffset
+        Title: string
+        Description: string
+        MinimumBid: decimal option
+    }
+
+type BidPlaced = 
+    {
+        Bidder: UserId
+        Amount: decimal
+        ReceivedOn: DateTimeOffset
+    }
+
